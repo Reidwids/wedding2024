3af1906c5b323f3224a91fed685e75c2666d20c8
Reidwids <reidwids@gmail.com>
2023-08-09 02:04:42 -0400

    try wrapping transporter

--- app/api/guests/rsvp/mailer.ts
+++ app/api/guests/rsvp/mailer.ts
@@ -27,26 +27,12 @@ export const sendRSVPConfirmedMail = async (email: string) => {
 
 		const transporter = await createTransporter();
 
-		await new Promise((resolve, reject) => {
-			// verify connection configuration
-			transporter.verify(function (error, success) {
-				if (error) {
-					console.log(error);
-					reject(error);
-				} else {
-					console.log("Server is ready to take our messages");
-					resolve(success);
-				}
-			});
-		});
-
-		await new Promise((resolve, reject) => {
+		return await new Promise((resolve, reject) => {
 			transporter.sendMail(options, (err, info) => {
 				if (err) {
 					console.error(err);
 					reject(err);
 				} else {
-					console.log(info);
 					resolve(info);
 				}
 			});
