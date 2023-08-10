f1a9594798e5fcff9a0581f59d678941c3bdfcac
Reidwids <reidwids@gmail.com>
2023-08-09 23:40:34 -0400

    move mailer modules into api folder

--- app/api/guests/all/route.ts
+++ app/api/guests/all/route.ts
@@ -1,9 +1,9 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
-// import { sendRSVPConfirmedMail } from "@/app/emails/mailer";
+import { sendRSVPConfirmedMail } from "../../emails/mailer";
 
 export async function GET() {
 	const guests = await prisma.guest.findMany();
-	// await sendRSVPConfirmedMail("reidwids");
+	await sendRSVPConfirmedMail("reidwids");
 	return new NextResponse(JSON.stringify(guests), { status: 200 });
 }
