0a8457284004e8886f93c3061f5e92e2592cf638
Reidwids <reidwids@gmail.com>
2023-09-12 23:31:13 -0400

    layout for FAQ page

--- app/(body)/faq/page.tsx
+++ app/(body)/faq/page.tsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
 export default function FAQ() {
-	return <div>FAQ</div>;
+	return (
+		<div className="max-w-4xl font-lora w-full ">
+			<div className="w-full flex justify-center">
+				<Image
+					alt="Text Divider"
+					src="/textDividerDainty.svg"
+					className="w-60 md:w-80 my-12 md:my-16"
+					width={200}
+					height={100}
+				/>
+			</div>
+			<div className="font-parisienne text-3xl md:text-4xl text-center">Frequently Asked Questions</div>
+			<ul className="list-disc list-outside mx-5 max-w-4xl px-5">
+				<li className="text-xl md:text-2xl my-4">
+					<span className="font-bold font-parisienne">What is the dress code?</span>
+					<br />
+					<span className="text-sm md:text-lg">The dress code is semi-formal. Please no jeans or t-shirts.</span>
+				</li>
+				<li className="text-xl md:text-2xl my-4">
+					<span className="font-bold font-parisienne">How long should we book out stay for?</span>
+					<br />
+					<span className="text-sm md:text-lg">
+						We will be staying from Wednesday to Tuesday, and we suggest to book for the same period - but if you'd like to
+						stay shorter feel free!
+					</span>
+				</li>
+			</ul>
+		</div>
+	);
 }
