002eecf4e28b7d806bd87acb7461eb3ca39204eb
Reidwids <reidwids@gmail.com>
2023-11-18 11:51:58 -0500

    fix google maps, update travel page

--- app/(body)/travel/Map.tsx
+++ app/(body)/travel/Map.tsx
@@ -1,13 +1,52 @@
 "use client";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
 export default function Map() {
-	return (
-		<iframe
-			title={"Iberostar Paraiso Beach"}
-			width="100%"
-			height="100%"
-			style={{ border: "0" }}
-			loading="lazy"
-			src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNYqhE7NDTo8RL4hjcI6etdg&key=${process.env.GMAPS_KEY}`}
-		></iframe>
+	const { isLoaded } = useLoadScript({
+		id: "google-map-script",
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_KEY!,
+	});
+	const containerStyle = {
+		width: "100%",
+		height: "100%",
+	};
+	const center = { lat: 20.761181010476665, lng: -86.96402858240013 };
+	return isLoaded ? (
+		<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+			{/* Child components, such as markers, info windows, etc. */}
+			<Marker position={center} />
+		</GoogleMap>
+	) : (
+		<></>
 	);
+
+	// const { isLoaded } = useLoadScript({
+	// 	id: "google-map-script",
+	// 	googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_KEY!,
+	// });
+
+	// function Map() {
+	// 	const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+	// 	return (
+	// 		<GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+	// 			<Marker position={center} />
+	// 		</GoogleMap>
+	// 	);
+	// }
+
+	// if (!isLoaded) return <div>Loading...</div>;
+	// return <Map />;
+
+	// return (
+	// 	<iframe
+	// 		title={"Iberostar Paraiso Beach"}
+	// 		width="100%"
+	// 		height="100%"
+	// 		style={{ border: "0" }}
+	// 		loading="lazy"
+	// 		src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNYqhE7NDTo8RL4hjcI6etdg&key=${process.env.GMAPS_KEY}`}
+	// 	></iframe>
+	// );
 }
--- app/(body)/travel/page.tsx
+++ app/(body)/travel/page.tsx
@@ -4,81 +4,129 @@ import Map from "./Map";
 
 export default function Travel() {
 	return (
-		<div className="w-full flex flex-col items-center font-lora px-3" style={{ boxShadow: "0 3px 3px #00000033 inset" }}>
-			<h1 className="text-4xl mt-5 font-roseritta">Travel Information</h1>
+		<div
+			className="w-full flex flex-col items-center font-lora px-3 mb-5"
+			style={{ boxShadow: "0 3px 3px #00000033 inset" }}
+		>
+			<h1 className="text-5xl mt-5 text-center font-roseritta">Travel Information</h1>
 			<h1 className="text-xl mt-1 font-lora">Booking your journey</h1>
-			<Image alt="Text Divider" src="/plane-trail2.svg" className="-rotate-[27deg] -mt-10 ml-16" width={200} height={100} />
-			<div className="mt-5">
-				<h3 className="text-2xl font-semibold">Overview</h3>
-				<p className="text-md max-w-2xl mt-2">
-					In the interest of keeping travel options flexible and economical, travel has not been predefined. Instead, will be
-					providing you with the best available options we have found, and will leave it up to you to compose your own
-					retreat!
-				</p>
-			</div>
-			<div className="mt-5">
-				<h3 className="text-2xl font-semibold">Location</h3>
-				<p className="text-md max-w-2xl mt-2">
-					The wedding will be held at Iberostar Paraiso Beach Resort in Riviera Maya. Paraíso Beach is located on the Playa
-					Paraíso complex grounds, which includes a total of 5 Iberostar All-Inclusive resorts. As guests of beach, we have
-					access to two resorts, both Paraiso beach and Paraiso Del Mar. Just remember when booking that to attend the
-					wedding you must be staying at Paraiso Beach!
-				</p>
-				<p className="text-md max-w-2xl mt-2">Avenida del Sol Km 309, Playa Paraiso, 77710 Riviera Maya, Q.R., Mexico</p>
-
-				<div className="h-80">
-					<Map />
-				</div>
-			</div>
-
-			<div className="mt-5">
-				<h3 className="text-2xl font-semibold">Timeline</h3>
-				<p className="text-md max-w-2xl mt-2">
-					We hope that everybody will be able to join us for week from{" "}
-					<span className="font-bold">Wednesday November 20th</span> to{" "}
-					<span className="font-bold">Wednesday November 27th</span>. - However, we understand that this may not be possible
-					for everyone. Please feel free to customize your dates however you'd like, just remember that the ceremony will be
-					held the afternoon of <span className="font-bold">Saturday November 23rd</span>.
-				</p>
-			</div>
-
-			<div className="mt-5">
-				<h3 className="text-2xl font-semibold">Booking Information</h3>
-				<p className="text-md max-w-2xl mt-2">
-					When booking, you have two options. You can book your flight and resort together through a vacation package
-					provider such as Sunwing, or you can book your flight and resort stay separately.
-				</p>
-				<div className="mt-5">
-					<h3 className="text-xl font-semibold">Seperate</h3>
-					<p className="text-md max-w-2xl mt-2">
-						Booking your flight and resort stay separately will give you the most flexibility in your travel plans, and will
-						likely be more economical. This will require you to do the following:
-						<ul className="list-decimal list-inside">
-							<li>
-								Book your flight. We leave you in the capable hands of{" "}
-								<a className="underline" href="https://www.google.com/travel/flights" target="_blank">
-									Google Flights
-								</a>
-								!
-							</li>
-
-							<li>
-								Book your resort stay. This will be directly on the{" "}
-								<a
-									className="underline"
-									href="https://www.iberostar.com/en-ca/hotels/riviera-maya/iberostar-paraiso-beach/"
-									target="_blank"
-								>
-									Iberostar Paraiso Beach Website
-								</a>
-								.
-							</li>
-							<li>Book your airport transfer. </li>
-						</ul>
+			<Image alt="Text Divider" src="/plane-trail2.svg" className="-rotate-[27deg] -mt-10 ml-16 " width={200} height={200} />
+			<div className="mt-5 max-w-3xl ">
+				<div className="md:mt-3">
+					<h3 className="text-2xl font-[500]">Overview</h3>
+					<p className="text-md mt-2">
+						In the interest of keeping travel options flexible and economical, travel has not been predefined. Instead, will
+						be providing you with the best available options we have found, and will leave it up to you to create your own
+						retreat!
 					</p>
 				</div>
-			</div>
+				<div className="mt-5">
+					<h3 className="text-2xl font-[500]">Location</h3>
+					<p className="text-md mt-2">
+						The wedding will be held at Iberostar Paraiso Beach Resort in Riviera Maya. Paraíso Beach is located on the Playa
+						Paraíso complex grounds, which includes a total of 5 Iberostar All-Inclusive resorts. As guests of beach, we have
+						access to two resorts, both Paraiso beach and Paraiso Del Mar. Just remember when booking that to attend the
+						wedding you must be staying at Paraiso Beach!
+					</p>
+				</div>
 
+				<div className="h-80 my-5">
+					<Map />
+					<p className="text-xs ">Avenida del Sol Km 309, Playa Paraiso, 77710 Riviera Maya, Q.R., Mexico</p>
+				</div>
+
+				<div className="mt-5">
+					<h3 className="text-2xl font-[500] pt-5">Timeline</h3>
+					<p className="text-md mt-2">
+						We hope that everybody will be able to join us for week from{" "}
+						<span className="font-bold">Wednesday November 20th</span> to{" "}
+						<span className="font-bold">Wednesday November 27th</span>. - However, we understand that this may not be
+						possible for everyone. Please feel free to customize your dates however you'd like, just remember that the
+						ceremony will be held the afternoon of <span className="font-bold">Saturday November 23rd</span>.
+					</p>
+				</div>
+
+				<div className="mt-5">
+					<h3 className="text-2xl font-[500]">Booking Information</h3>
+					<p className="text-md mt-2">
+						When booking, you have two options. You can book your flight and resort together through a vacation package
+						provider such as Sunwing, or you can book the flight and resort separately.
+					</p>
+					<div className="mt-5">
+						<h3 className="text-xl font-[500]">Seperate</h3>
+						<div className="text-md mt-2">
+							Booking your flight and resort stay separately will give you the most flexibility in your travel plans, and
+							will likely be more economical. This will require you to do the following:
+							<ul className="list-decimal list-inside">
+								<li>
+									Book your flight. We leave you in the capable hands of{" "}
+									<a className="underline" href="https://www.google.com/travel/flights" target="_blank">
+										Google Flights
+									</a>
+									!
+								</li>
+
+								<li>
+									Book your resort stay. This will be directly on the{" "}
+									<a
+										className="underline"
+										href="https://www.iberostar.com/en-ca/hotels/riviera-maya/iberostar-paraiso-beach/"
+										target="_blank"
+									>
+										Iberostar Paraiso Beach Website
+									</a>
+									.
+								</li>
+								<li>Book your airport transfer.</li>
+							</ul>
+						</div>
+					</div>
+					<div className="mt-5">
+						<h3 className="text-xl font-[500]">Vacation Package</h3>
+						<div className="text-md mt-2">
+							Booking your stay through a vacation package provider can be more convenient, and will typically provide you
+							with a complete travel itinerary. This will require you to do the following:
+							<ul className="list-decimal list-inside">
+								<li>
+									Choose your desired vacation package provider. Ensure they offer packages specifically for Iberostar
+									Paraiso Beach - We suggest using{" "}
+									<a
+										className="underline"
+										href="https://www.sunwing.ca/en/hotel/mexico/riviera-maya/iberostar-paraiso-beach"
+										target="_blank"
+									>
+										Sunwing
+									</a>
+									!
+								</li>
+
+								<li>
+									Book your vacation. Ensure the flight, resort stay, and airport transfer are all included in the package.
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="mt-5">
+						<h3 className="text-xl font-[500]">Finishing Steps</h3>
+						<div className="text-md mt-2">
+							<div>
+								Once you have completed booking your booking, please let us know what your travel plans are! We will be
+								putting together a list of everyone's booking and submitting it to the resort in efforts to put all of our
+								rooms in the same area. Please send me an email at{" "}
+								<a className="underline" href="mailto:reidwids@gmail.com">
+									reidwids@gmail.com
+								</a>{" "}
+								with the following information:
+								<ul className="list-disc list-inside">
+									<li>The primary name associated to your booking with the resort</li>
+									<li>The dates you will be staying at the resort</li>
+									<li>Your flight arrival and departure times</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 			{/* 
 
 			Title of section: Travel Information
@@ -99,8 +147,6 @@ export default function Travel() {
 
 
 		Please email us after you have booked with the name you've booked the trip under
-		
-		
 		*/}
 		</div>
 	);
