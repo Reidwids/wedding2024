f4b3d69abc68ef6e7577035bf8a251a86b99b6a1
Reidwids <reidwids@gmail.com>
2024-01-07 21:31:43 -0500

    dynamic get all gets

--- app/api/guests/all/route.ts
+++ app/api/guests/all/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
 	const guests = await prisma.guest.findMany();
 	return new NextResponse(JSON.stringify(guests), { status: 200 });
