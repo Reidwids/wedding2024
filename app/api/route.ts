18274b315ec55fd11b4a66c5a6721f6baa5432a4
reidwids <reidwids@gmail.com>
2024-01-28 12:55:22 -0500

    test commit 2

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-// test git
 export async function GET(request: Request) {
 	return new NextResponse(JSON.stringify({ message: "Wedding API Functional", status: 200 }), { status: 200 });
 }
