e8736a98362fee4a1172c095b78128d0a3be86f9
reidwids <reidwids@gmail.com>
2024-06-19 23:27:05 -0400

    add dress examples

--- app/(body)/dress-code/page.tsx
+++ app/(body)/dress-code/page.tsx
@@ -1,5 +1,5 @@
-import TextDivider from "@/app/components/TextDivider";
 import React from "react";
+import Image from "next/image";
 
 export default function DressCode() {
 	return (
@@ -26,19 +26,10 @@ export default function DressCode() {
 						{["#A78749", "#E6E1CB", "#C1A888", "#ECEFF1", "#f7e7ce", "#E1C9A4", "#D1CEC2"].map((color, i) => (
 							<div
 								key={i}
-								className="w-10 h-10 rounded-full mx-2 border-[1px] border-[#999]"
+								className="w-7 h-7 sm:w-10 sm:h-10  rounded-full mx-2 border-[1px] border-[#999]"
 								style={{ backgroundColor: color }}
 							/>
 						))}
-					</div>
-				</div>
-				<div>
-					<h1 className="text-3xl mt-5 text-center font-roseritta">Women</h1>
-					<div>
-						A formal evening dress, a dressy cocktail dress, or a chic pantsuit in a light neutral color such as any shade of
-						nude, sandy beige, or grey tones, champagnes, silvers and golds are encouraged. If you don't feel comfortable in
-						a color, black is also acceptable. We kindly request that you avoid patterns to maintain a cohesive and elegant
-						atmosphere.
 					</div>
 				</div>
 				<div>
@@ -50,6 +41,23 @@ export default function DressCode() {
 						well as pocket squares, suspenders, ties, bowties, and/or any other accessories. We kindly request that you avoid
 						patterns to maintain a cohesive and elegant aesthetic.
 					</div>
+				</div>
+				<div className="flex flex-col items-center">
+					<h1 className="text-3xl mt-5 text-center font-roseritta">Women</h1>
+					<div>
+						A formal evening dress, a dressy cocktail dress, or a chic pantsuit in a light neutral color such as any shade of
+						nude, sandy beige, or grey tones, champagnes, silvers and golds are encouraged. If you don't feel comfortable in
+						a color, black is also acceptable. We kindly request that you avoid patterns to maintain a cohesive and elegant
+						atmosphere.
+					</div>
+					<Image
+						key={"selected-image"}
+						src={"/dresses.png"}
+						alt="Womens Dresses"
+						width={300}
+						height={400}
+						className="px-5 py-5 max-w-lg w-full"
+					/>
 				</div>
 			</div>
 		</div>
